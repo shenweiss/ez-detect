69f4589b7ad62225d2ec6341030ff53fe7ae51e8
Tomas Pastore <tpastore@dc.uba.ar>
2018-12-12 00:22:20 -0300

    improved a bit the code in evt_metrics

--- test/eventFiles_test.py
+++ test/eventFiles_test.py
@@ -18,7 +18,7 @@ class eventFilesTest(unittest.TestCase):
         self.E_events = read_events(self.expected_fn)
 
     def test_similar_event_files(self):
-        self.assertTrue( evt_metrics.distance(self.O_events, self.E_events) <= self.delta) 
+        self.assertTrue( evt_metrics.distance(self.O_events, self.E_events) <= self.delta- self.delta/4) 
 
     def test_obtained_is_subset(self):
         self.assertTrue( evt_metrics.subset(self.O_events, self.E_events, self.delta) )
@@ -27,14 +27,15 @@ class eventFilesTest(unittest.TestCase):
         self.assertTrue( evt_metrics.subset(self.E_events, self.O_events, self.delta) )
 
     #Will be removed after some time
-    #def test_binary_match(self):
-    #    self.assertEqual(evt_metrics.proportion_of_naive(self.O_events, self.E_events),
-    #                     evt_metrics.proportion_of(self.O_events, self.E_events))
-    #    self.assertEqual(evt_metrics.proportion_of_naive(self.E_events, self.O_events),
-    #                      evt_metrics.proportion_of(self.E_events, self.O_events))
+    def test_binary_match(self):
+        self.assertEqual(evt_metrics.proportion_of_naive(self.O_events, self.E_events),
+                         evt_metrics.proportion_of(self.O_events, self.E_events))
+        self.assertEqual(evt_metrics.proportion_of_naive(self.E_events, self.O_events),
+                          evt_metrics.proportion_of(self.E_events, self.O_events))
 
     def test_print_metrics(self):
-        evt_metrics.print_metrics(self.obtained_fn, self.expected_fn, self.delta) 
+        if self.trc_fname != 'NOT_GIVEN':
+            evt_metrics.print_metrics(self.obtained_fn, self.expected_fn, self.trc_fname, self.delta) 
         pass
 
 if __name__ == "__main__":
@@ -48,28 +49,28 @@ if __name__ == "__main__":
                         help="Sets the filename of the eventfile to be tested",
                         required=True)
 
-    eventFilesTest.delta = 0.1 #10% is default. Means that at most 10% of the events in the smaller set
-                                 #may not match with an event in the other set.
+    parser.add_argument("-trc", "--trc_fname", 
+                        help="Sets the filename of the tracefile that was runned in both "+
+                        "versions, it is used to extract the channel list.",
+                        required=False, default='NOT_GIVEN')
+
 
     parser.add_argument("-d", "--delta", 
                         help="The two set of events will be tested to be equal "+
-                        "with a delta tolerance, meaning that at most 10 percent of "+
-                        "the events in the smaller set may not match with an "+
-                        "event in the other set."
-                        "Default is: "+ str(eventFilesTest.delta),
-                        required=False)   
+                        "with a delta tolerance, meaning that at most 100*delta percent of "+
+                        "the events in one set may not match with an "+
+                        "event in the other set.",
+                        required=False, default= 0.1, type=float)   
 
     args = parser.parse_args()
     
     eventFilesTest.expected_fn = args.expected
     eventFilesTest.obtained_fn = args.obtained
+    eventFilesTest.trc_fname = args.trc_fname
+    eventFilesTest.delta = float(args.delta)
 
-    if(args.delta):
-        eventFilesTest.delta = float(args.delta)
+    while(len(sys.argv) > 1):
         sys.argv.pop()
-
-    sys.argv.pop()
-    sys.argv.pop()
     unittest.main()
 
 
--- test/evt_metrics.py
+++ test/evt_metrics.py
@@ -59,20 +59,11 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath('../src/main'))
 from evtio import read_events
+from trcio import read_raw_trc
 
 MIN_DATETIME = datetime.min.replace(tzinfo=tzutc())
 MAX_DATETIME = datetime.max.replace(tzinfo=tzutc())
 
-#temp until we get montage from trc
-import scipy.io
-def loadChansFromMontage(trc_filename, chans_num):
-
-    filename = os.path.basename(trc_filename)
-    filename = os.path.splitext(filename)[0]
-    montage_filename = os.path.expanduser("~") + '/hfo_engine_1/montages/' + filename + '_montage.mat'
-    aDic = scipy.io.loadmat(montage_filename)
-    chanlist = [aDic['montage'][i][0][0] for i in range(chans_num) ]
-    return chanlist
 
 # INPUT: an increasingly ordered by begin interval list: [(begin, end)], 
 #        aBegin is a number as the ones in the intervals
@@ -203,7 +194,7 @@ def min_proportion(proportions_by_chan, min_chan_tot=1):
     min_m, min_tot = INFINITY, 1 #the first will replace it
     for chan, p in proportions_by_chan.items():
         matches, tot = p
-        if tot > min_chan_tot and matches/tot < min_m/min_tot:
+        if tot >= min_chan_tot and matches/tot < min_m/min_tot:
             min_m, min_tot = matches, tot
 
     return min_m, min_tot
@@ -230,66 +221,50 @@ def distance(A, B):
     A_min_p = matches_A / tot_A
     B_min_p = matches_B / tot_B
    
-    return 2 - (A_min_p + B_min_p)
+    return 1 - (A_min_p + B_min_p) / 2 
  
-#INPUT: Two evt filenames, O is obtained and E is expected
-# delta is the tolerance for similarity tests.
-
-#como hago para que un path pueda ser ingresado como relativo o absoluto (shell)
-def print_metrics(obtained_fn, expected_fn, delta=0.1):
-    O_events = read_events(obtained_fn)
-    E_events = read_events(expected_fn)
-
-    obtained_basename = splitext(basename(obtained_fn))[0] 
-    expected_basename = splitext(basename(expected_fn))[0] 
-    O_tot = sum([len(evts) for evts in O_events.values()])
-    E_tot = sum([len(evts) for evts in E_events.values()])
-
-    #Highlighting max, make a function later
-    if(O_tot > E_tot): 
-        o_tot_str = '\x1b[6;30;42m' + str(O_tot) + '\x1b[0m'
+def highlight_max(n, m):
+    if(n > m): 
+        n_str = '\x1b[6;30;42m' + str(n) + '\x1b[0m'
     else:
-        o_tot_str = str(O_tot)
+        n_str = str(n)
 
-    if(E_tot > O_tot): 
-        e_tot_str = '\x1b[6;30;42m' + str(E_tot) + '\x1b[0m'
+    if(m > n): 
+        m_str = '\x1b[6;30;42m' + str(m) + '\x1b[0m'
     else:
-        e_tot_str = str(E_tot)
+        m_str = str(m)
 
-    original_chanlist = loadChansFromMontage('/home/tomas-pastore/hfo_engine_1/449_correct.trc', 66)
+    return n_str, m_str
+
+def print_count_by_channel(O_events, E_events, original_chanlist, obtained_basename, expected_basename):
+    
     opened_by_chan= ['Channel Name', 
                      'Channel ID', 
                      'Event count in ' + obtained_basename,
                      'Event count in ' + expected_basename]
-    
     rows_chan_count = []
     for i in range(len(original_chanlist)):
-
         chan_name = original_chanlist[i]
         chan_id = i+1
         o_evt_count = 0 if chan_id not in O_events.keys() else len(O_events[chan_id]) 
         e_evt_count = 0 if chan_id not in E_events.keys() else len(E_events[chan_id])
-        
-        #Highlighting max, make a function later
-        if(o_evt_count > e_evt_count): 
-            o_evt_count_str = '\x1b[6;30;42m' + str(o_evt_count) + '\x1b[0m'
-        else:
-            o_evt_count_str = str(o_evt_count)
-        
-        if(e_evt_count > o_evt_count): 
-            e_evt_count_str = '\x1b[6;30;42m' + str(e_evt_count) + '\x1b[0m'
-        else:
-            e_evt_count_str = str(e_evt_count)
-        
+        o_evt_count_str, e_evt_count_str = highlight_max(o_evt_count, e_evt_count)           
         r_i_chan_count = [chan_name, str(chan_id), o_evt_count_str, e_evt_count_str ]
         rows_chan_count.append(r_i_chan_count)
+
     sep = '---------------------'
     rows_chan_count.append([sep, sep, sep, sep])
+
+    O_tot = sum([len(evts) for evts in O_events.values()])
+    E_tot = sum([len(evts) for evts in E_events.values()])
+    o_tot_str, e_tot_str = highlight_max(O_tot, E_tot)
     rows_chan_count.append(['Total count', str(" "), o_tot_str, e_tot_str])
+
     print("\nEvent count by channel...")
     print("\n"+tabulate( rows_chan_count, headers=opened_by_chan, tablefmt='orgtbl'))
 
-    print("\nProportions of one in the other...")
+def print_proportions(O_events, E_events, original_chanlist, obtained_basename, expected_basename):
+
     proportions= ['Channel Name', 
                   'Channel ID', 
                   'Proportion of ' + obtained_basename + " events\n that are also in "+ expected_basename,
@@ -299,47 +274,40 @@ def print_metrics(obtained_fn, expected_fn, delta=0.1):
     E_prop_by_chan = proportion_of(E_events, O_events)
     rows_chan_prop = []
     for i in range(len(original_chanlist)):
-
         chan_name = original_chanlist[i]
         chan_id = i+1
         o_evt_prop = 100.0 if chan_id not in O_events.keys() else (O_prop_by_chan[chan_id][0]/O_prop_by_chan[chan_id][1]) * 100 
         e_evt_prop = 100.0 if chan_id not in E_events.keys() else (E_prop_by_chan[chan_id][0]/E_prop_by_chan[chan_id][1]) * 100
-        
-        #Highlighting max, make a function later
-        if(o_evt_prop > e_evt_prop): 
-            o_evt_prop_str = '\x1b[6;30;42m' + str(o_evt_prop) + '\x1b[0m'
-        else:
-            o_evt_prop_str = str(o_evt_prop)
-        
-        if(e_evt_prop > o_evt_prop): 
-            e_evt_prop_str = '\x1b[6;30;42m' + str(e_evt_prop) + '\x1b[0m'
-        else:
-            e_evt_prop_str = str(e_evt_prop)
-        
+        o_evt_prop_str, e_evt_prop_str = highlight_max(o_evt_prop, e_evt_prop)
         r_i_chan_prop = [chan_name, str(chan_id), o_evt_prop_str, e_evt_prop_str ]
         rows_chan_prop.append(r_i_chan_prop)
+
     sep = '---------------------'
     rows_chan_prop.append([sep, sep, sep, sep])
    
     O_matches, O_chan_events = min_proportion(O_prop_by_chan, min_chan_tot=1)
     E_matches, E_chan_events = min_proportion(E_prop_by_chan, min_chan_tot=1)
-    #Highlighting max, make a function later
-    
-    obt_prop_exp = O_matches/O_chan_events
-    exp_prop_obt = E_matches/E_chan_events
-
-    if(obt_prop_exp > exp_prop_obt): 
-        obt_prop_exp_str = '\x1b[6;30;42m' + str(obt_prop_exp) + '\x1b[0m'
-    else:
-        obt_prop_exp_str = str(obt_prop_exp)
-    
-    if(exp_prop_obt > obt_prop_exp): 
-        exp_prop_obt_str = '\x1b[6;30;42m' + str(exp_prop_obt) + '\x1b[0m'
-    else:
-        exp_prop_obt_str = str(exp_prop_obt)
-
+    obt_prop_exp_min = O_matches/O_chan_events
+    exp_prop_obt_min = E_matches/E_chan_events
+    obt_prop_exp_str, exp_prop_obt_str = highlight_max(obt_prop_exp_min, exp_prop_obt_min) 
     rows_chan_prop.append(['Minimum proportion', str(" "), obt_prop_exp_str, exp_prop_obt_str])
+
+    print("\nProportions of one in the other...")
     print("\n"+tabulate( rows_chan_prop, headers=proportions, tablefmt='orgtbl'))
+
+#INPUT: Two evt filenames, O is obtained and E is expected,
+# The trc_fname where to get the channel names
+# delta is the tolerance for similarity tests.
+#como hago para que un path pueda ser ingresado como relativo o absoluto (shell)
+def print_metrics(obtained_fn, expected_fn, trc_fname, delta=0.1):
+    obtained_basename = splitext(basename(obtained_fn))[0] 
+    expected_basename = splitext(basename(expected_fn))[0] 
+    original_chanlist = read_raw_trc(trc_fname, preload=True).info['ch_names']
+
+    O_events = read_events(obtained_fn)
+    E_events = read_events(expected_fn)
+    print_count_by_channel(O_events, E_events, original_chanlist, obtained_basename, expected_basename)
+    print_proportions(O_events, E_events, original_chanlist, obtained_basename, expected_basename)
 
     subset_res = " is a subset of " if subset(O_events, E_events) else " is not a subset of " 
     print("\nWith a tolerance of "+ str(delta*100) + " percent, "+ obtained_basename + subset_res + expected_basename)
@@ -348,6 +316,6 @@ def print_metrics(obtained_fn, expected_fn, delta=0.1):
     print("With a tolerance of "+ str(delta*100) + " percent, "+ expected_basename + subset_res + obtained_basename)
 
     dist = distance(O_events, E_events)
-    print("\nThe distance between the event files is: "+ str(dist/2))
+    print("\nThe distance between the event files is: "+ str(dist))
 
 
